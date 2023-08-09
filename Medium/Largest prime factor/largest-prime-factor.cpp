ba57d41201c26db8e5df39317a411b28b64fa882
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-08-09 18:15:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest prime factor/largest-prime-factor.cpp
+++ Medium/Largest prime factor/largest-prime-factor.cpp
@@ -3,28 +3,20 @@
 using namespace std;
 
 // } Driver Code Ends
-
-const int Ni=1e5+10;
-vector<bool> is_prime(Ni,true);
-vector<int> highest_prime(Ni,0);
-vector<int> lowest_prime(Ni,0);
 class Solution{
 public: 
-    long long int largestPrimeFactor(int n){
-        is_prime[0]=is_prime[1]=false;
-     for(int i=2;i<Ni;i++){
-        if(is_prime[i]==true){
-            highest_prime[i]=lowest_prime[i]=i;
-            for(int j=2*i;j<Ni;j+=i){
-                is_prime[j]=false;
-                if(lowest_prime[j]==0){
-                    lowest_prime[j]=i;
-                }
-                highest_prime[j]=i;
+    long long int largestPrimeFactor(int N){
+
+        long long  ans=N;
+        for(int i=2;i*i<=N;i++){
+            if(N>0&&N%i==0)
+            ans=i;
+            while(N>0&&N%i==0){
+                N=N/i;
             }
         }
-    }
-        return highest_prime[n];
+      if(N>1) return N;
+       return ans;
     }
 };
 
