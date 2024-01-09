30ad44d58b38ace92d182be9a6ea2beb9fccf9bb
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2024-01-09 23:01:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        
+        void precomputeFunction(vector<int> &precomputeArr, string &s2)
+        {
+            int len = 0;
+            int index = 1;
+            int m = s2.size();
+            while (index < m)
+            {
+                if (s2[len] == s2[index])
+                {
+                    len++;
+                    precomputeArr[index] = len;
+                    index++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        len = precomputeArr[len - 1];
+                    }
+                    else
+                    {
+                        precomputeArr[index] = 0;
+                        index++;
+                    }
+                }
+            }
+        }
+        
+        vector <int> search(string s2, string s1)
+        {
+            int count = 0;
+            int n = s1.size();
+            int m = s2.size();
+            vector<int> precomputeArr(m, 0);
+            precomputeFunction(precomputeArr, s2);
+            int i = 0;
+            int j = 0;
+            vector<int> ans;
+            while (i < n)
+            {
+                if (s1[i] == s2[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    if (j == 0)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        j = precomputeArr[j - 1];
+                    }
+                }
+                if (j == m)
+                {
+                    count++;
+                    ans.push_back(i - j + 1);
+                    j = precomputeArr[j - 1];
+                }
+            }
+            return ans;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
