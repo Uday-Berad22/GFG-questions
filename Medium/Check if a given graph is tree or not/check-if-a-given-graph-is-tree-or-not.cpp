86372b197f4a12ff162816ca439d3355a83a4702
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2024-01-25 10:45:04 +0530

    Updated Solution - GfG to GitHub

--- Medium/Check if a given graph is tree or not/check-if-a-given-graph-is-tree-or-not.cpp
+++ Medium/Check if a given graph is tree or not/check-if-a-given-graph-is-tree-or-not.cpp
@@ -36,33 +36,51 @@ public:
 // } Driver Code Ends
 class Solution {
   public:
-     bool isCycle(vector<vector<int>>& adj, vector<int>& vis, int n, int m, int node, int parent) {
-        vis[node]=1;
-        for (auto newNode : adj[node]) {
-            if (!vis[newNode]) {
-                if (!isCycle(adj, vis, n, m, newNode, node)) {
-                    return false;
+  
+    bool isCycle(int i,vector<vector<int>> &graph,vector<int> &stk,vector<int> &visited,int prevnbr,int &count,vector<int> &num){
+        count++;
+        for(auto &nbr: graph[i]){
+            if(visited[nbr]==true&&stk[nbr]==false){
+                count+=num[nbr];
+            }
+            
+            else if(visited[nbr]==true&&stk[nbr]==true&&nbr!=prevnbr) return true;
+            
+            else if(visited[nbr]==false)
+                {
+                    visited[nbr]=true;
+                    stk[nbr]=true;
+                    if( isCycle(nbr,graph,stk,visited,i,count,num)) return true;
+                    stk[nbr]=false;
                 }
-            } else if (newNode != parent) {
-                return false;
+        }
+        
+        return false;
+    }
+    int isTree(int n, int m, vector<vector<int>> &adj) {
+        if(n==1&&m==0) return 1;
+        vector<vector<int>> graph(n);
+        vector<int> visited(n,false);
+        vector<int> stk(n,false);
+        for(auto &a: adj){
+            graph[a[0]].push_back(a[1]);
+            graph[a[1]].push_back(a[0]);
+            if(a[0]==a[1]) return 0;
+        }
+        int maxi=0;
+        vector<int> num(n,0);
+        for(int i=0;i<n;i++){
+            if(visited[i]!=true){
+                int count=0;
+                visited[i]=true;
+                stk[i]=true;
+                if(isCycle(i,graph,stk,visited,-1,count,num)) return 0;
+                stk[i]=false;
+                num[i]=count;
+                maxi=max(count,maxi);
             }
         }
-        return true;
-    }
-
-    int isTree(int n, int m, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n, vector<int>());
-        for (int i = 0; i < m; i++) {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-        }
-    
-        vector<int> vis(n,0);
-        bool res = isCycle(adj, vis, n, m, 0, -1);
-        for(int i=0;i<n;i++){
-            if(!vis[i]) return false;
-        }
-        return res;
+        return maxi==n?1:0;
     }
 };
 
