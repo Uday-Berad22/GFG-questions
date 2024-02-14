9c53c909400d706d3195057aa47c956a65eb7c4f
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2024-02-14 12:28:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -7,55 +7,30 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
+	
+	void toposortDFS(int i,int V,vector<int> adj[],vector<bool> &visited,stack<int> &stk) {
+	    visited[i]=true;
+	    for(int j=0;j<adj[i].size();j++){
+	        if(!visited[adj[i][j]]){
+	            toposortDFS(adj[i][j],V,adj,visited,stk);
+	        }
+	    }
+	    stk.push(i);
+	}
+	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    unordered_map<int,int> m;
-	    for(int i=0;i<V;i++){
-	    for(auto a: adj[i]){
-	        m[a]++;
-	       // m[a[0]]+=0;
-	       //cout<<a<<" "<<i<<endl;
-	    }
-	    }
-	    vector<pair<int,int> > temp;
 	    vector<int> ans;
-	     for(int i=0;i<V;i++){
-	       m[i]+=0;
+	    stack<int> stk;
+	    vector<bool> visited(V,false);
+	    for(int i=0;i<V;i++)
+    	    if(!visited[i])
+    	        toposortDFS(i,V,adj,visited,stk);
+	    while(!stk.empty()){
+	        ans.push_back(stk.top());
+	        stk.pop();
 	    }
-	    for(auto a:m ){
-	        temp.push_back({a.second,a.first});
-	    }
-	    sort(temp.begin(),temp.end());
-	   // for(int i=0;i<V;i++){
-	   //    // cout<<temp[i].first<<" "<<temp[i].second<<endl;
-	   //     ans.push_back(temp[i].second);
-	   //    // cout<<ans[i]<<" ";
-	   // }
-	   vector<bool> visited(V,false);
-        queue<int> q;
-	    for(int i=0;i<temp.size();i++){
-	         if(temp[i].first==0){
-	             q.push(temp[i].second);
-	         }
-	    }
-    	    while(!q.empty())
-            {
-                int node=q.front();
-                q.pop();
-                // cout<<node<<" ";
-                ans.push_back(node);
-                visited[node]=true;
-                for(auto a: adj[node]){
-                    if(!visited[a]&&m[a]<=1){
-                        m[a]--;
-                        q.push(a);
-                    }else{
-                        m[a]--;
-                    }
-                }
-            }
-	   // int start
 	    return ans;
 	}
 };
