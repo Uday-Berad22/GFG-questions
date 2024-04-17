ffd746912b7fb43595daa2a70d187343dd2d5fed
Uday Berad <120621657+Uday-Berad22@users.noreply.github.com>
2024-04-17 13:48:55 +0530

    Updated Solution - GfG to GitHub

--- Hard/Count Pairs in an Array/count-pairs-in-an-array.cpp
+++ Hard/Count Pairs in an Array/count-pairs-in-an-array.cpp
@@ -4,28 +4,74 @@ using namespace std;
 
 // } Driver Code Ends
 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
-  
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> 
-  
 
 class Solution{
     public:
+    void merge(int arr[],int n,int si,int mid,int ei,int &count){
+         vector<int> leftPart;
+         for(int i=si;i<=mid;i++){
+             leftPart.push_back(arr[i]);
+         }
+         vector<int> rightPart;
+         for(int i=mid+1;i<=ei;i++){
+             rightPart.push_back(arr[i]);
+         }
+         int i=si;
+         int j=0;
+         int k=0;
+         int c1=0;
+         int c2=0;
+        while(j<leftPart.size() && k<rightPart.size()){
+            if(leftPart[j]<=rightPart[k]){
+                arr[i]=leftPart[j];
+                j++;
+            }
+            else{
+                arr[i]=rightPart[k];
+                count+=(leftPart.size()-j);
+                k++;
+            }
+            i++;
+        }
+        while(j<leftPart.size()){
+            arr[i]=leftPart[j];
+            j++;
+            i++;
+        }
+        while(k<rightPart.size()){
+            arr[i]=rightPart[k];
+            k++;
+            i++;
+        }
+        // count+=(c1*c2);
+    }
+    void MergeSort(int arr[],int n,int si,int ei,int &count){
+        if(si>=ei){
+            return;
+        }
+        int mid=(si+ei)/2;
+        MergeSort(arr,n,si,mid,count);
+        MergeSort(arr,n,mid+1,ei,count);
+        merge(arr,n,si,mid,ei,count);
+    }
     int countPairs(int arr[] , int n ) 
     {
         // Your code goes here   
-        
-        ordered_set s;
-        int count=0;
-        for(int i=n-1;i>=0;i--){
-            s.insert(arr[i]*i);
-            count+=s.order_of_key(arr[i]*i);
+        for(int i=0;i<n;i++){
+            arr[i]*=i;
         }
+        int si=0;
+        int ei=n-1;
+        int count=0;
+        MergeSort(arr,n,si,ei,count);
         return count;
     }
 };
+
+// 0 0 20 6 16 5 36
+
+    //   3  1 1 
+
 
 //{ Driver Code Starts.
 // Driver code
