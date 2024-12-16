72a406a38c0c48e814be26bf1ec2d876d17479ae
Uday Berad <120621657+Uday-Berad22@users.noreply.github.com>
2024-12-16 10:02:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        // code here
+        int i=0;
+        int j=0;
+        int x=a[0];
+        int y=b[0];
+        while(k>0){
+            x=INT_MAX;
+            y=INT_MAX;
+            if(i<a.size()) x=a[i];
+            if(j<b.size()) y=b[j];
+            if(x>y){
+                j++;
+            }
+            else{
+                i++;
+            }
+            k--;
+        }
+        return min(x,y);
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
